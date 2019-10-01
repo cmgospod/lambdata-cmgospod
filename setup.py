8ba1090fbd9dcb96bf24160a285a447c52386d7f
cmgospod <cmgospod@users.noreply.github.com>
2019-10-01 18:00:25 -0500

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+REQUIRED = [
+    "numpy",
+    "pandas"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+    setuptools.setup(
+    name="lambdata-cmgospod",
+    version = "0.1.2",
+    author = "cmgospod",
+    description = "a collection of data science helper functions",
+    long_description = LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cmgospod/lambdata-cmgospod",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires = REQUIRED,
+    classifiers=["Programming Language :: Python :: 3",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    ]
+    )
